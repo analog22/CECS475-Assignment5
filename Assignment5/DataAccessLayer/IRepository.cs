8b7aa18da7077f2e40a8e38e132c67578b9872fa
Matthew Kim <matthew.kim64@gmail.com>
2017-11-05 23:02:05 -0800

    wtf am i doing
    lol

--- Assignment5/DataAccessLayer/IRepository.cs
+++ Assignment5/DataAccessLayer/IRepository.cs
@@ -8,14 +8,19 @@ namespace DataAccessLayer
     public interface IRepository<T> : IDisposable
     {
         void Insert(T entity);
-
+        /*
         T GetSingle(Func<T, bool> where, params Expression<Func<T, object>>[] navigationProperties);
-    }
 
-    class DataAccessLayer
-    {
-        static void Main(string[] args)
-        {
-        }
+        void Delete(T entity);
+
+        void Update(T entity);
+
+        T GetById(int id);
+
+        IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate);
+
+        IEnumerable<T> GetAll();
+        */
+        T GetSingle(Func<T, bool> where, params Expression<Func<T, object>>[] navigationProperties);
     }
 }
---
+++ Assignment5/DataAccessLayer/Repository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataAccessLayer
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected DbContext context;
+        protected DbSet<T> dbset;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+            dbset = context.Set<T>();
+        }
+
+        public void Insert(T entity)
+        {
+            context.Entry(entity).State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public T GetSingle(Func<T, bool> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            T item = null;
+            IQueryable<T> dbQuery = context.Set<T>();
+
+            //Apply eager loading
+            foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+            item = dbQuery
+                .AsNoTracking() //Don't track any changes for the selected item
+                .FirstOrDefault(where); //Apply where clause
+
+            return item;
+        }
+    }
+}
