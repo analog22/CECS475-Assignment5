030fa54afbe8e6dc2dd9a13c8b376d93d40f542c
Matthew Kim <matthew.kim64@gmail.com>
2017-11-05 17:52:34 -0800

    startup
    idk

---
+++ Assignment5/DataAccessLayer/DataAccessLayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataAccessLayer
+{
+    public interface IRepository<T> : IDisposable
+    {
+        void Insert(T entity);
+
+        T GetSingle(Func<T, bool> where, params Expression<Func<T, object>>[] navigationProperties);
+    }
+
+    class DataAccessLayer
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
